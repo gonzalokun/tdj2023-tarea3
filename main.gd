9fe77ebb7622902ecf657606306f5a7bd169d1e2
gonzalokun <glspringhart@gmail.com>
2023-11-06 23:51:52 -0300

    Actualizado script

--- main.gd
+++ main.gd
@@ -54,15 +54,29 @@ func _on_sim_torta_mandar_output(output):
 
 func escribir_output(data):
 	
-	var nombre_ar:String = "data/"+ nvar.text + "_" + tvar.text + "_" + str(Time.get_ticks_msec()) + ".csv"
+	var info_p = ""
+	
+	if infoPerf.button_pressed:
+		info_p = "perfecta"
+	
+	var j2_malvado = ""
+	
+	if j2m.button_pressed:
+		j2_malvado = "malvado"
+	
+	var nombre_ar:String = "data/" + nvar.text + "_" + tvar.text + "_" + str(Time.get_ticks_msec()) + "_" + info_p + "_" + j2_malvado + ".csv"
+	
 	var archivo_out = FileAccess.open(nombre_ar, FileAccess.WRITE)
 	
-	archivo_out.store_string("n,t,res_j1,res_j2,ganancia_j1,ganancia_j2\n")
+	archivo_out.store_string("nro_partida,t,res_j1,res_j2,ganancia_j1,ganancia_j2\n")
+	
+	var nro_partida = 1
 	
 	for item in data:
-		archivo_out.store_string(nvar.text + "," 
-		+ tvar.text + "," + str(item[0]) + ","
-		+ str(item[1]) + "," + str(item[2]) + ","
-		+ str(item[3]) + "\n")
+		archivo_out.store_string(str(nro_partida) + "," + tvar.text + "," 
+		+ str(item[0]) + "," + str(item[1]) + "," 
+		+ str(item[2]) + "," + str(item[3]) + "\n")
+		
+		nro_partida += 1
 	
 	archivo_out.close()
